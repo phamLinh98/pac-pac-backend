b0c5596e973861cc09fba407f74044befecedb1c
phamLinh98 <linhthusinh98@gmail.com>
2024-12-28 23:21:52 +0530

    fix query list

--- models/listModel.js
+++ models/listModel.js
@@ -1,9 +1,6 @@
 export const getList = () => {
-    const query = `SELECT * FROM list`;
-    // const values = [];
-    // const queryObject = {
-    //     query,
-    //     values
-    // }
+    const query = `SELECT l.*,u.name AS user_name,u.avatar AS avatar
+                   FROM list l
+                   JOIN "public"."user" u ON l.user_id = u.id`;
     return query;
 }
