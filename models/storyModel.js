bf2d0bc6738d33ccbb5c4d79cbc59655d748d9cc
phamLinh98 <linhthusinh98@gmail.com>
2024-12-28 23:54:35 +0530

    fix query story

--- models/storyModel.js
+++ models/storyModel.js
@@ -1,9 +1,7 @@
 export const getStory = () => {
-    const query = `SELECT * FROM story`;
-    // const values = [];
-    // const queryObject = {
-    //     query,
-    //     values
-    // }
+    const query = `SELECT l.*,u.name AS user_name,u.avatar AS avatar
+    FROM story l
+    JOIN "public"."user" u ON l.user_id = u.id`;
+return query;
     return query;
 }
