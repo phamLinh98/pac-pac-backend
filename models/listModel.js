b4fd4d258f8a99422eeaec47f319dfefc0a8f903
phamLinh98 <linhthusinh98@gmail.com>
2024-12-29 00:35:40 +0530

    update query list

--- models/listModel.js
+++ models/listModel.js
@@ -1,6 +1,24 @@
 export const getList = () => {
-    const query = `SELECT l.*,u.name AS user_name,u.avatar AS avatar
-                   FROM list l
-                   JOIN "public"."user" u ON l.user_id = u.id`;
+    const query = `WITH updated_list AS (
+    UPDATE list
+    SET comment = (
+        SELECT COUNT(*)
+        FROM comment c
+        WHERE c.post_id = list.id
+    )
+    RETURNING *
+)
+SELECT
+    l.id,
+    l.user_id,
+    l.content,
+    l.comment,
+    l.created_at,
+    u.name AS user_name,
+    u.avatar AS avatar
+FROM
+    updated_list l
+JOIN
+    "public"."user" u ON l.user_id = u.id;`;
     return query;
 }
