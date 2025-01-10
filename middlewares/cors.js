562aa983a3d2da274ccf43b3cbc99d2fb13734c6
phamLinh98 <linhthusinh98@gmail.com>
2025-01-11 02:00:38 +0530

    add cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,6 @@
 import cors from "cors";
 
 const allowedOrigins = [
-    "https://pac-pac-backend.vercel.app",
     "http://localhost:5173/login",
     "http://localhost:5173",
     "http://localhost:3000"
