f0a31263b1018fc8da70fafaef8ca05eac253ff2
phamLinh98 <linhthusinh98@gmail.com>
2025-01-13 01:43:29 +0530

    verify token

--- middlewares/checkTokenNotValid.js
+++ middlewares/checkTokenNotValid.js
@@ -1,9 +1,19 @@
 // tokenMiddleware.js
+import jwt from 'jsonwebtoken';
+import { envConfig } from '../configs/envConfig.js';
+
 export const checkTokenMiddleware = (req, res, next) => {
-    const accessToken = req.signedCookies?.accessToken;
-    console.log('accessToken', accessToken)
-    if (!accessToken) {
-      return res.status(401).json({ error: "Không có AccessToken, quyền truy cập bị từ chối" });
+  const accessToken = req.signedCookies?.accessToken;
+  console.log('accessToken', accessToken)
+  if (!accessToken) {
+    return res.status(401).json({ error: "Không có AccessToken, quyền truy cập bị từ chối" });
+  }
+
+  jwt.verify(accessToken, envConfig.accessSecretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Access token không hợp lệ' });
     }
+    req.user = decoded; // Lưu thông tin user vào req.user
     next();
-  };
+  });
+};
