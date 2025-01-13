d24d5ed1033ab5682add3b87a28863507bfef363
phamLinh98 <linhthusinh98@gmail.com>
2025-01-13 20:59:07 +0530

    cmt

--- middlewares/checkTokenNotValid.js
+++ middlewares/checkTokenNotValid.js
@@ -4,7 +4,6 @@ import { envConfig } from '../configs/envConfig.js';
 
 export const checkTokenMiddleware = (req, res, next) => {
   const accessToken = req.signedCookies?.accessToken;
-  console.log('accessToken', accessToken)
   if (!accessToken) {
     return res.status(401).json({ error: "Không có AccessToken, quyền truy cập bị từ chối" });
   }
