d747a3f3fb52bd72dec9d8094742f7ac64bf354b
phamLinh98 <linhthusinh98@gmail.com>
2025-01-13 23:35:12 +0530

    add routers

--- routers/router.js
+++ routers/router.js
@@ -10,11 +10,11 @@ const router = express.Router();
 router.get('/list', checkTokenMiddleware,listController.getList);
 router.get('/list/:id', checkTokenMiddleware, listController.getListStatusOfOneUser);
 
-router.get('/story', storyController.getStory);
-router.get('/comment', commentController.getComment);
-router.get('/comment/:id', commentController.getCommentByListId);
+router.get('/story',checkTokenMiddleware,  storyController.getStory);
+router.get('/comment', checkTokenMiddleware, commentController.getComment);
+router.get('/comment/:id', checkTokenMiddleware, commentController.getCommentByListId);
 
-router.get('/user', userController.getUser);
+router.get('/user', checkTokenMiddleware, userController.getUser);
 router.post('/login', userController.loginUserByEmailAndPassword);
 router.get('/logout', userController.logoutAndRemoveAllToken);
 router.get('/refesh-token', userController.refreshTokenWhenExpired);
