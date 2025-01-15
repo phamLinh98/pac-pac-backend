9c34e94163e2d2b20eccd5ecac08d64229608b15
PhamTuanLinh <58585884+phamLinh98@users.noreply.github.com>
2025-01-16 00:24:05 +0530

    fix logic cors (#1)

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,12 @@
 import cors from "cors";
 
-const allowedOrigins = 'http://localhost:5173'
+const allowedOrigins = ['http://localhost:5173', 'https://pac-pac-backend.vercel.app']; // Thêm các origin được phép
 
 const corsOptions = {
-    origin: allowedOrigins,
-    credentials: true
+    origin: allowedOrigins, // Cho phép các origin cụ thể
+    credentials: true, // Cần thiết khi sử dụng cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Các phương thức được phép
+    allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'], // Headers được phép
 };
 
 const corsMiddleware = cors(corsOptions);
