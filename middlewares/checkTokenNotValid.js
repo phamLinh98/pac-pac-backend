8df6c7f876ed9034eaf3c52aa3894c5784829627
phamLinh98 <linhthusinh98@gmail.com>
2025-01-16 03:07:37 +0530

    fix middleware

--- middlewares/checkTokenNotValid.js
+++ middlewares/checkTokenNotValid.js
@@ -1,6 +1,6 @@
 // tokenMiddleware.js
 export const checkTokenMiddleware = (req, res, next) => {
-  const accessToken = req.signedCookies?.accessToken;
+  const accessToken = req.cookies.accessToken;
   if (!accessToken) {
     return res.status(404).json({ error: "AccessToken không tồn tại, quyền truy cập bị từ chối" });
   }
