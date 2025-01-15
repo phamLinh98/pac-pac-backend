995ab2f169e0e69c7fb4220a110a9d11e92f2982
phamLinh98 <linhthusinh98@gmail.com>
2025-01-16 03:39:50 +0530

    add logic check token is ok

--- middlewares/checkTokenNotValid.js
+++ middlewares/checkTokenNotValid.js
@@ -1,17 +1,22 @@
+import { envConfig } from "../configs/envConfig";
+
 // tokenMiddleware.js
 export const checkTokenMiddleware = (req, res, next) => {
   const accessToken = req.signedCookies ? req.signedCookies.accessToken : undefined;
   console.log('accessToken', req.signedCookies.accessToken);
   if (!accessToken) {
-    return res.status(404).json({ error: "AccessToken không tồn tại, quyền truy cập bị từ chối" });
+    return res.status(401).json({ error: "AccessToken không tồn tại, quyền truy cập bị từ chối" });
   }
-  jwt.verify(accessToken, secretKey, (err, decoded) => {
+  jwt.verify(accessToken, envConfig.accessToken, async (err, decoded) => {
     if (err) {
-      console.error('Token không hợp lệ:', err.message);
-      return res.status(403).json({ error: "Token không hợp lệ, quyền truy cập bị từ chối" });
+      return res.status(403).json({ error: 'Token không hợp lệ, quyền truy cập bị từ chối' });
     }
-    // Nếu token hợp lệ, thêm thông tin user vào req
-    req.user = decoded; // decoded chứa payload từ token
-    next(); // Tiếp tục xử lý request
+    // // Kiểm tra userId có tồn tại trong database
+    // const user = await User.findById(decoded.userId);
+    // if (!user) {
+    //   return res.status(403).json({ error: 'User không tồn tại, quyền truy cập bị từ chối' });
+    // }
+    req.user = decoded; // Gắn user vào req để sử dụng ở controller
+    next();
   });
 };
