97f09ee7b8ef49312af105724d83cc50335bb1be
phamLinh98 <linhthusinh98@gmail.com>
2025-01-16 03:51:24 +0530

    add cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,18 +1,9 @@
 import cors from "cors";
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:3000'
-];
+const allowedOrigins = 'http://localhost:5173'
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: allowedOrigins,
     credentials: true
 };
 
