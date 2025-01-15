6541c1f2dd04a6a32f9773c0a178500f43e6e78c
phamLinh98 <linhthusinh98@gmail.com>
2025-01-16 02:50:58 +0530

    refact checkTokenMiddleware

--- middlewares/checkTokenNotValid.js
+++ middlewares/checkTokenNotValid.js
@@ -1,18 +1,7 @@
 // tokenMiddleware.js
-import jwt from 'jsonwebtoken';
-import { envConfig } from '../configs/envConfig.js';
-
 export const checkTokenMiddleware = (req, res, next) => {
   const accessToken = req.signedCookies?.accessToken;
   if (!accessToken) {
     return res.status(401).json({ error: "Không có AccessToken, quyền truy cập bị từ chối" });
-  }
-
-  jwt.verify(accessToken, envConfig.accessSecretKey, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: 'Access token không hợp lệ' });
-    }
-    req.user = decoded; // Lưu thông tin user vào req.user
-    next();
-  });
+  } next();
 };
