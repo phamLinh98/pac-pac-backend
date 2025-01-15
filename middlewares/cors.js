f6173a3ccea9f025cb731e8822628dc15af13021
phamLinh98 <linhthusinh98@gmail.com>
2025-01-16 03:49:21 +0530

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,16 +1,16 @@
 import cors from "cors";
 
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000"
+    'http://localhost:5173',
+    'http://localhost:3000'
 ];
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
-            callback(new Error("Not allowed by CORS"));
+            callback(new Error('Not allowed by CORS'));
         }
     },
     credentials: true
