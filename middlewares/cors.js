fa3bab7a53675c80185967bba4f717020ff9d7e6
phamLinh98 <linhthusinh98@gmail.com>
2025-01-17 00:46:46 +0530

    cors fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,13 @@ import cors from "cors";
 const allowedOrigins = ['http://localhost:5173']; // Thêm các origin được phép
 
 const corsOptions = {
-    origin: allowedOrigins, // Cho phép các origin cụ thể
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // cho phép các request không có origin ( ví dụ: server to server request)
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }, 
     credentials: true, // Cần thiết khi sử dụng cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Các phương thức được phép
     allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie','Origin'], // Headers được phép
