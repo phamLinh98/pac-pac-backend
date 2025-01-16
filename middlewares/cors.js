65c0ac95b320364be2b54106612659dcde312bff
phamLinh98 <linhthusinh98@gmail.com>
2025-01-17 00:33:15 +0530

    fix cors origin

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,7 +6,7 @@ const corsOptions = {
     origin: allowedOrigins, // Cho phép các origin cụ thể
     credentials: true, // Cần thiết khi sử dụng cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Các phương thức được phép
-    allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'], // Headers được phép
+    allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie','Origin'], // Headers được phép
 };
 
 const corsMiddleware = cors(corsOptions);
